c355cd86a264110458a7ed0f7991f8be21d52a92
OsFlash <felipedasilvatomaz1996@gmail.com>
2023-09-14 23:52:30 -0300

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/'
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? process.env.PUBLIC_URL
+      : 'http://localhost:3000/'
 })
